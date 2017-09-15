c7ef28d661fb409f6b82f11bc2e55f15820a3abc
gagarinbefree <gagarinbefree@gmail.com>
2017-09-15 11:23:27 +0500

    Add project files.

---
+++ VskEntities/CompoundProduct.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VskEntities
+{
+    public class CompoundProduct : IProduct
+    {        
+        List<IProduct> Children { get; set; } = new List<IProduct>();
+
+        public int Price { get; set; }
+
+        public int CalcPrice()
+        {
+            int price = 0;
+            foreach(IProduct child in Children)
+            {
+                price += child.Price;
+            }
+
+            return price;
+        }
+
+        public void Add(IProduct entity)
+        {
+            Children.Add(entity);
+        }
+        public void Remove(IProduct entity)
+        {
+            Children.Remove(entity);
+        }
+
+    }
+}
